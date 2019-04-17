eba44d0bd87a85f661b3cd3579df3c83f5f77d71
Roch <roch.sherriff@academytrainee.com>
2019-04-17 14:37:07 +0100

    prototype navbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>QA games</h1>
+        <HashRouter>
+          <ul className="navbar">
+            <li><NavLink to ="/game1" className="navlink">game1</NavLink></li>
+            <li><NavLink to ="/game2" className="navlink">game2</NavLink></li>
+            <li><NavLink to ="/game3" className="navlink">game3</NavLink></li>
+            <li><NavLink to ="/game4" className="navlink">game4</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route path="/game1" render={()=><h2>game 1</h2>}/>
+            <Route path="/game2" render={()=><h2>game 2</h2>}/>
+            <Route path="/game3" render={()=><h2>game 3</h2>}/>
+            <Route path="/game4" render={()=><h2>game 4</h2>}/>
+          </div>
+        </HashRouter>
       </div>
     );
   }
